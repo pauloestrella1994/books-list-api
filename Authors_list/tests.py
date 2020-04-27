1eebee7c6fbec5a05f2f1894519a41b2bd66acc9
Paulo Netto <pauloestrella1994@gmail.com>
2020-04-27 00:49:42 -0300

    [11] - Commit - Creating Tests with Pytest

--- Authors_list/tests.py
+++ Authors_list/tests.py
@@ -1,3 +1,41 @@
 from django.test import TestCase
+import requests
 
-# Create your tests here.
+
+class TestAuthors:
+    url_base = 'http://127.0.0.1:8000/authors_list/'
+
+    def test_get_authors(self):
+        authors_get = requests.get(url=self.url_base)
+
+        assert authors_get.status_code == 200
+
+    def test_get_author(self):
+        author_get = requests.get(url=f'{self.url_base}6/')
+
+        assert author_get.status_code == 200
+
+    def test_post_author(self):
+        author_post = {"author_name": "Luciano Ramalho"}
+        result = requests.post(url=self.url_base, data=author_post)
+
+        assert result.status_code == 201
+        assert result.json()['author_name'] == author_post['author_name']
+
+    def test_put_author(self):
+        author_put = {"author_name": "Luciano Ramalho 2"}
+        result = requests.put(url=f'{self.url_base}6/', data=author_put)
+        
+        assert result.status_code == 200
+        assert result.json()['author_name'] == author_put['author_name']
+    
+    def test_delete_author(self):
+        result = requests.delete(url=f'{self.url_base}15/')
+
+        assert result.status_code == 404 and len(result.text) == 0
+
+    
+
+
+
+
--- core_list/tests.py
+++ core_list/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+import requests
 
-# Create your tests here.
+
+class TestAuthors:
+    url_base = 'http://127.0.0.1:8000/books_list/'
+
+    def test_get_books(self):
+        books_get = requests.get(url=self.url_base)
+
+        assert books_get.status_code == 200
+
+    def test_get_book(self):
+        book_get = requests.get(url=f'{self.url_base}1/')
+
+        assert book_get.status_code == 200
+
+    def test_post_book(self):
+        book_post = {"name": "Python Fluente", "edition": 1,"publication_year": 2005}
+        result = requests.post(url=self.url_base, data=book_post)
+
+        assert result.status_code == 201
+        assert result.json()['name'] == book_post['name']
+
+    def test_put_book(self):
+        book_put = {"name": "Python Fluente 2", "edition": 1,"publication_year": 2005}
+        result = requests.put(url=f'{self.url_base}1/', data=book_put)
+        
+        assert result.status_code == 200
+        assert result.json()['name'] == book_put['name']
+    
+    # def test_delete_book(self):
+    #     result = requests.delete(url=f'{self.url_base}15/')
+
+    #     assert result.status_code == 404 and len(result.text) == 0
